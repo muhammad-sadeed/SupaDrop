639869c95d714beea10c1096f04e2d7c7f77595f
Sadeed <sadeed@Sadeeds-MacBook-Air.local>
2025-08-11 11:48:31 +0500

    added code input for file download

--- src/routes/download/+page.svelte
+++ src/routes/download/+page.svelte
@@ -1,3 +1,30 @@
+<script>
+    import { supabase } from "../supabase-client";
+    let inputCode;
+    let downloadUrl;
+
+    const handleFileDownload = async () => {
+        const {data, error} = await supabase
+            .from("uploaded-files")
+            .select("file_url")
+            .eq("generated_code", inputCode)
+
+        if (error) {
+            console.error("Error while fetching file: ", error.message)
+            return
+        }
+
+        downloadUrl = data[0].file_url;
+    }
+</script>
+
 <main>
     <h1>Download a File</h1>
+    <div class="download-section">
+        <input type="text" bind:value={inputCode}>
+        <button onclick={handleFileDownload}>Download</button>
+    </div>
+    <h2>
+        {downloadUrl}
+    </h2>
 </main>
