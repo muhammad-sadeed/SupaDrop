54d125c063b66cb4338750d07297fd1a1905c7fe
Sadeed <sadeed@Sadeeds-MacBook-Air.local>
2025-08-11 09:21:16 +0500

    added upload and download page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Home Page</h1>
+
+<nav>
+    <li>
+        <a href="/upload">upload</a>
+    </li>
+    <li>
+        <a href="/download">download</a>
+    </li>
+</nav>
