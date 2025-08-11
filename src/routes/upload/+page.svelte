c8aba72e67bd13f5f1ee19938b9a056724d0417b
Sadeed <sadeed@Sadeeds-MacBook-Air.local>
2025-08-11 10:23:28 +0500

    added a function for uploading files

--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -1,3 +1,39 @@
+<script>
+    import { supabase } from "../supabase-client";
+    let uploadedFile;
+    let fileUrl;
+
+    const uploadFile = async (file) => {
+        let filePath = `${file.name}`
+        
+        const {error} = await supabase.storage
+            .from("supa-files")
+            .upload(filePath, file);
+
+        if (error) {
+            console.error("Error Uploading File: ", error.message)
+            return null
+        }
+ 
+        const {data} = await supabase.storage
+            .from("supa-files")
+            .getPublicUrl(filePath)
+
+        return data.publicUrl;
+    }
+
+    const handleFileUpload = async () => {
+        if (uploadedFile) {
+            fileUrl = await uploadFile(uploadedFile[0]);
+        }
+    }
+</script>
+
 <main>
-    <h1>Upload Page</h1>
+    <h1>Upload you file</h1>
+    <div class="upload-section">
+        <input type="file" bind:files={uploadedFile} onchange={() => console.log(uploadedFile[0].name)}>
+        <button onclick={handleFileUpload}>upload</button>
+    </div>
+    {fileUrl}
 </main>
